1118ed0b94a15bca8fc2a12da9e89e89646c01a4
wesfly <wesleydeanford@yahoo.com>
2025-02-02 20:10:34 +0100

    improved day accuracy, still off

--- age_calculator.py
+++ age_calculator.py
@@ -7,9 +7,9 @@ def calculate_age(birth_date_str):
         delta = today - birth_date
         if delta.days < 0:
             raise ValueError("Invalid date. Please enter a valid birthdate.")
-        years = delta.days // 365
-        months = (delta.days % 365) // 30
-        days = (delta.days % 30)
+        years = delta.days // 365.25
+        months = (delta.days % 365.25) // 30.4
+        days = (delta.days % 30.4)
         return years, months, days
     except ValueError as e:
         error = str(e).split()
@@ -24,7 +24,7 @@ def main():
     try:
         years, months, days = calculate_age(birth_date_input)
         if years and months and days != None:
-            print(f"Years: {years}, Months: {months}, Days: {days}")
+            print(f"Years: {int(years)}, Months: {int(months)}, Days: {int(days)}")
     except ValueError as e:
         print("Enter a valid date. All numbers, YYYY-MM-DD format.")
 
